8844d0e6afb6840474d350f6e8446e7e47eb0fda
Daniel Owen van Dommelen <9572060+TheApeMachine@users.noreply.github.com>
2021-08-08 12:11:09 +0200

    Me for once not being an idiot

--- debug.go
+++ debug.go
@@ -6,37 +6,25 @@ import (
 )
 
 /* Panic is a pretty printed panic line. */
-func (collector Collector) Panic(msg string) { pterm.Fatal.Println(msg); panic(nil) }
+func (collector Collector) Panic(msg ...interface{}) { pterm.Fatal.Println(msg); panic(nil) }
 
 /* Fatal is a pretty printed fatal line. */
-func (collector Collector) Fatal(msg string) { pterm.Fatal.Println(msg) }
+func (collector Collector) Fatal(msg ...interface{}) { pterm.Fatal.Println(msg) }
 
 /* Critical is a pretty printed critical line. */
-func (collector Collector) Critical(msg *string) {
-	if msg == nil {
-		return
-	}
-
-	pterm.Error.Println(*msg)
-}
+func (collector Collector) Critical(msg ...interface{}) { pterm.Error.Println(msg) }
 
 /* Error is a pretty printed error line. */
-func (collector Collector) Error(msg *string) {
-	if msg == nil {
-		return
-	}
-
-	pterm.Error.Println(*msg)
-}
+func (collector Collector) Error(msg ...interface{}) { pterm.Error.Println(msg) }
 
 /* Info is a pretty printed info line. */
-func (collector Collector) Info(msg string) { pterm.Info.Println(msg) }
+func (collector Collector) Info(msg ...interface{}) { pterm.Info.Println(msg) }
 
 /* Warn is a pretty printed warning line. */
-func (collector Collector) Warn(msg string) { pterm.Warning.Println(msg) }
+func (collector Collector) Warn(msg ...interface{}) { pterm.Warning.Println(msg) }
 
 /* Debug is a pretty printed info line. */
-func (collector Collector) Debug(msg string) {
+func (collector Collector) Debug(msg ...interface{}) {
 	// Don't print noise if we are not in debug mode.
 	if !viper.GetBool("debug") {
 		return
